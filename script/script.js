1e2a60f019692412f911c8d3ae9b14c00a7f2a0a
Erik-Ga <115992708+Erik-Ga@users.noreply.github.com>
2023-04-12 10:55:26 +0200

    hej

--- script/script.js
+++ script/script.js
@@ -1,32 +1,24 @@
 drawMap()
 function drawMap() {
-    const row = 5;
-    const col = 5;
-    let map = new Array(row); // create an empty array of length n
-    for (var i = 0; i < row; i++) {
-        map[i] = new Array(col); // make each element an array
-    }
-    console.log(arr); //  Output: [ [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ] ]
-    
+
     let table = "";
     table += "<table>"
     let nr = 0;
-
+    const row = 5;
+    const col = 5;
+    let map = new Array(row); // create an empty array of length n
     for (let x = 0; x < row; x++) {
         table += "<tr>"
 
         for (y = 0; y < col; y++) {
-            nr++
             const spot = document.createElement("td")
-            table += "<td>" + (nr) + "</td>"
-            spot.setAttribute("id", nr)
-
+            table += "<td>" + (map[y] = new Array(col)) + "</td>"
         }
 
         table += "</tr>"
     }
     table += "</table>"
-
+    console.log(arr);
     document.getElementById("map").innerHTML = table
 }
 
