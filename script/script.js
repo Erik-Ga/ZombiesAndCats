74dd0616bc13629786ddfbe43ed4c2dc133ac777
eemmaaS <115992713+eemmaaS@users.noreply.github.com>
2023-04-12 13:56:07 +0200

    lego

--- script/script.js
+++ script/script.js
@@ -1,30 +1,33 @@
-drawMap()
+let map = [
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""]
+];
+
 function drawMap() {
     let table = "";
     table += "<table>"
-    let nr = 0;
 
-    const row = 5;
-    const col = 5;
-    let map = new Array(row); // create an empty array of length n
-    for (var i = 0; i < row; i++) {
-        map[i] = new Array(col); // make each element an array
-    }
     console.log(map); //  Output: [ [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ] ]
 
     for (let x = 0; x < 5; x++) {
         table += "<tr>"
 
         for (y = 0; y < 5; y++) {
-            table += "<td>" + (map[i] = new Array(col)) + "</td>"
-            map[1,1] = "X"
+            table += "<td>" + (map[x][y]) + "</td>"
         }
-
         table += "</tr>"
     }
+
+    
     table += "</table>"
 
     document.getElementById("map").innerHTML = table
 }
+
+
+drawMap()
 
 
