d8a61760c4b577c1dc76e3070758ee1a37cffc40
Erik-Ga <115992708+Erik-Ga@users.noreply.github.com>
2023-04-12 14:42:02 +0200

    wtf

--- script/script.js
+++ script/script.js
@@ -1,33 +1,70 @@
-let map = [
-    ["","","","",""],
-    ["","","","",""],
-    ["","","","",""],
-    ["","","","",""],
-    ["","","","",""]
+let array = [
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""]
 ];
 
-function drawMap() {
-    let table = "";
-    table += "<table>"
+var currentRow = Math.floor(Math.random() * 5) + 1; // Current row
+var currentCol = Math.floor(Math.random() * 5) + 1; // Current column
 
-    console.log(map); //  Output: [ [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ] ]
-
-    for (let x = 0; x < 5; x++) {
-        table += "<tr>"
-
-        for (y = 0; y < 5; y++) {
-            table += "<td>" + (map[x][y]) + "</td>"
+// Function to display the array in HTML
+function displayArray() {
+    var table = "<table>";
+    for (var i = 0; i < array.length; i++) {
+        table += "<tr>";
+        for (var j = 0; j < array[i].length; j++) {
+            if (i === currentRow && j === currentCol) {
+                table += "<td class='current-pos'>" + "X" + "</td>";
+            } else {
+                table += "<td>" + array[i][j] + "</td>";
+            }
         }
-        table += "</tr>"
+        table += "</tr>";
+        document.getElementById("array-container").innerHTML = table;
     }
-
-    
-    table += "</table>"
-
-    document.getElementById("map").innerHTML = table
+    table += "</table>";
+    document.getElementById("array-container").innerHTML = table;
 }
 
+// Function to update the current row and column
+function updateCurrentPos(row, col) {
+    array[currentRow][currentCol] = array[currentRow][currentCol].toString().replace("X", ""); // Revert last position marker
+    currentRow = row;
+    currentCol = col;
+    array[currentRow][currentCol] = "X" + array[currentRow][currentCol].toString(); // Update new position with marker
+    document.getElementById("current-pos").innerHTML = "Current Position: Row " + (currentRow + 1) + ", Column " + (currentCol + 1);
+    displayArray(); // Redraw the array with the new current position
+}
 
-drawMap()
+// Function to navigate up in the array
+function navigateUp() {
+    if (currentRow > 0) {
+        updateCurrentPos(currentRow - 1, currentCol);
+    }
+}
 
+// Function to navigate down in the array
+function navigateDown() {
+    if (currentRow < array.length - 1) {
+        updateCurrentPos(currentRow + 1, currentCol);
+    }
+}
+
+// Function to navigate left in the array
+function navigateLeft() {
+    if (currentCol > 0) {
+        updateCurrentPos(currentRow, currentCol - 1);
+    }
+}
+
+// Function to navigate right in the array
+function navigateRight() {
+    if (currentCol < array[currentRow].length - 1) {
+        updateCurrentPos(currentRow, currentCol + 1);
+    }
+}
+
+updateCurrentPos(currentRow, currentCol)
 
