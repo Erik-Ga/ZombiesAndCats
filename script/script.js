55a55c06bdc5a6af512f5def98fe8acd34be2698
eemmaaS <115992713+eemmaaS@users.noreply.github.com>
2023-04-12 11:06:45 +0200

    push for erk

--- script/script.js
+++ script/script.js
@@ -1,30 +1,30 @@
 drawMap()
 function drawMap() {
-
     let table = "";
     table += "<table>"
     let nr = 0;
+
     const row = 5;
     const col = 5;
     let map = new Array(row); // create an empty array of length n
-    for (let x = 0; x < row; x++) {
+    for (var i = 0; i < row; i++) {
+        map[i] = new Array(col); // make each element an array
+    }
+    console.log(map); //  Output: [ [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ] ]
+
+    for (let x = 0; x < 5; x++) {
         table += "<tr>"
 
-        for (y = 0; y < col; y++) {
-            const spot = document.createElement("td")
-            table += "<td>" + (map[y] = new Array(col)) + "</td>"
+        for (y = 0; y < 5; y++) {
+            table += "<td>" + (map[i] = new Array(col)) + "</td>"
+            map[1,1] = "X"
         }
 
         table += "</tr>"
     }
     table += "</table>"
-    console.log(arr);
-    document.getElementById("map").innerHTML = table
-}
 
-function test() {
-    document.getElementById("7").innerHTML = table
-    spot.setAttribute("value", "0")
+    document.getElementById("map").innerHTML = table
 }
 
 
