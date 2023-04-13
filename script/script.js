a41a5f7e5b93d428af439baa6fae055ed7368fcf
eemmaaS <115992713+eemmaaS@users.noreply.github.com>
2023-04-13 13:37:27 +0200

    zombie moving yay

--- script/script.js
+++ script/script.js
@@ -9,11 +9,10 @@ let array = [
 var currentRow = Math.floor(Math.random() * 5); // Current row
 var currentCol = Math.floor(Math.random() * 5); // Current column
 
-let zombieRow = Math.floor(Math.random() * 5)
-let zombieCol = Math.floor(Math.random() * 5)
-
 window.onload = displayArray;
-zombies()
+zombiesAnKitties();
+
+
 
 // Function to display the array in HTML
 function displayArray() {
@@ -34,6 +33,7 @@ function displayArray() {
     document.getElementById("array-container").innerHTML = table;
     document.getElementById("current-pos").innerHTML = "Current Position: Row " + (currentRow + 1) + ", Column " + (currentCol + 1);
     document.getElementById("zombie-pos").innerHTML = "Zombie Position: Row " + (zombieRow + 1) + ", Column " + (zombieCol + 1);
+    document.getElementById("kitty-pos").innerHTML = "Kitty Position: Row " + (kittyRow + 1) + ", Column " + (kittyCol + 1);
 }
 
 // Function to update the current row and column
@@ -43,18 +43,57 @@ function updateCurrentPos(row, col) {
     currentCol = col;
     array[currentRow][currentCol] = "X" + array[currentRow][currentCol].toString(); // Update new position with marker
     document.getElementById("current-pos").innerHTML = "Current Position: Row " + (currentRow + 1) + ", Column " + (currentCol + 1);
+    
+
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j].charAt(0) === "Z") {
+                if (i > currentRow) {
+                    if (array[i - 1][j] === "") {
+                        array[i - 1][j] = "Z";
+                        array[i][j] = "";
+                        
+                    }
+                } else if (i < currentRow) {
+                    if (array[i + 1][j] === "") {
+                        array[i + 1][j] = "Z";
+                        array[i][j] = "";
+                    }
+                } else if (j > currentCol) {
+                    if (array[i][j - 1] === "") {
+                        array[i][j - 1] = "Z";
+                        array[i][j] = "";
+                    }
+                } else if (j < currentCol) {
+                    if (array[i][j + 1] === "") {
+                        array[i][j + 1] = "Z";
+                        array[i][j] = "";
+                    }
+                }
+            }
+        }
+    }
+    
+    
     displayArray(); // Redraw the array with the new current position
 }
-function zombies()
-{
-    zombieRow   
-    for(let z = 0; z < 2; z++)
-    {
+function zombiesAnKitties() {
+    for (let z = 0; z < 2; z++) {
         array[currentRow][currentCol] = array[currentRow][currentCol].toString().replace("X", "");
+        let zombieRow = Math.floor(Math.random() * 5)
+        let zombieCol = Math.floor(Math.random() * 5)
         array[zombieRow][zombieCol] = "Z" + array[zombieRow][zombieCol].toString(); // Update new position with marker
-        
+
     }
+    for (let k = 0;k < 2; k++) {
+        let kittyRow = Math.floor(Math.random() * 5)
+        let kittyCol = Math.floor(Math.random() * 5)
+        array[kittyRow][kittyCol] = "K" + array[kittyRow][kittyCol].toString(); // Update new position with marker
+    }
+    
     document.getElementById("zombie-pos").innerHTML = "Zombie Position: Row " + (zombieRow + 1) + ", Column " + (zombieCol + 1);
+    document.getElementById("kitty-pos").innerHTML = "Kitty Position: Row " + (kittyRow + 1) + ", Column " + (kittyCol + 1);
     displayArray();
 }
 
